fd655bf39d4bfd1885fa79a7eb18a1514f523a57
AndreBomfim99 <andre.bomfim99@gmail.com>
2024-11-05 14:45:48 -0300

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,3 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     // Aqui você pode adicionar interatividade com JavaScript
     console.log("Página carregada!");
 });
+
+// Aqui você pode adicionar interatividade com JavaScript
